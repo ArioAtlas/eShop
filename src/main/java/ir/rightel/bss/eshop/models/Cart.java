2d0a725ff278665a659b760b670a30b213cbfda0
Omid Golshan <o.golshan@rightel.ir>
2020-02-21 23:28:51 +0330

    add cart models and service interface

---
+++ src/main/java/ir/rightel/bss/eshop/models/Cart.java
@@ -0,0 +1,15 @@
+package ir.rightel.bss.eshop.models;
+
+import java.util.List;
+
+public class Cart {
+    private List<CartItem> items;
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+}
